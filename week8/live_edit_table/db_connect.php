02ff59ade3a869e1c91a6dad2a6a16383e1f9ddc
l-kaif-l <kaifahmed1052004@gmail.com>
2024-09-26 09:36:04 +0530

    updates

--- week8/live_edit_table/db_connect.php
+++ week8/live_edit_table/db_connect.php
@@ -1,16 +1,11 @@
 <?php
-$servername = "127.0.0.1";
+$host = "localhost";
+$user = "root";
+$password = "708090";
+$database = "test_db";
 
-$username = "root";  // Replace with your MySQL username
-$password = "708090";      // Replace with your MySQL password (leave blank if no password)
-$dbname = "live_edit_db"; // Database name
-
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-$conn = new mysqli($servername, $username, $password, $dbname, 3303); // Use correct port
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$conn = mysqli_connect($host, $user, $password, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
